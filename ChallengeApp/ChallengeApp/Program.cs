c5d89845883d305721eca75575acbc62b3d383b0
Maria <maria.r.wach@gmail.com>
2023-12-07 07:53:00 +0100

    4 dzień v3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,16 @@
-﻿string imię = "Tomek";
-var płeć = "Mężczyzna";
-int wiek = 16;
+﻿string name = "Tomek";
+var sex = "Mężczyzna";
+int age = 16;
 
-if (płeć == "Kobieta" && wiek < 30)
+if (sex == "Kobieta" && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (imię == "Ewa" && wiek == 33)
+else if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-else if (płeć != "Kobieta" && wiek < 18)
+else if (sex != "Kobieta" && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
