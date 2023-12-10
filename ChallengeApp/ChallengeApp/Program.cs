f0910a37666084d3be44c6ef81670279731aaff6
Maria <maria.r.wach@gmail.com>
2023-12-10 18:24:46 +0100

    5 dzień (licznik, foreach) v1

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,69 @@
-﻿string name = "Tomek";
-var sex = "Mężczyzna";
-int age = 16;
+﻿int number = 3310;
+string numberAsString = number.ToString();
+char[] signs = numberAsString.ToArray();
 
-if (sex == "Kobieta" && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char sign in signs)
+{ 
+    if(sign == '0')
+    {
+        counter0++;
+    }
+    else if(sign == '1')
+    {
+        counter1++;
+    }
+    else if(sign == '2')
+    {
+        counter2++;
+    }
+    else if(sign == '3')
+    {
+        counter3++;
+    }
+    else if (sign == '4')
+    {
+        counter4++;
+    }
+    else if (sign == '5')
+    {
+        counter5++;
+    }
+    else if (sign == '6')
+    {
+        counter6++;
+    }
+    else if (sign == '7')
+    {
+        counter7++;
+    }
+    else if (sign == '8')
+    {
+        counter8++;
+    }
+    else if (sign == '9')
+    {
+        counter9++;
+    }
 }
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (sex != "Kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
